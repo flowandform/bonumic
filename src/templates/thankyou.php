e97687c3372f6a8067b49ad7e0603fcd40f8a8e8
Mile Balenovic <mile@flownform.com>
2020-11-30 17:08:33 +0100

    update 404 & thankyou page

--- src/templates/thankyou.php
+++ src/templates/thankyou.php
@@ -6,56 +6,23 @@ Template Name: ThankYou Page
 <?php
 get_header()
 ?>
-
-<main class="about-main thankyou-main">
-  <div class="header container">
-    <div class="title-wrapper">
-      <div class="title"> <span style='color:#2d13e3'>Thank you</span> for</br>getting in touch.</div>
-      <p class="block__description wrapper">
-        We will get back to you soon.
-      </p>
-
-    </div>
-  </div>
-  <div class="home-wrapper">
-    <a href="/" class="home-wrapper__button">HOMEPAGE</a>
-  </div>
-</main>
-<section class="blog" id="blog">
-  <section class="container">
-    <section class="blog__posts posts posts--front-page">
-      <?php
-      $args = array(
-        'post_type' => 'post',
-        'post__not_in' => array($activePostId),
-        'orderby'    => 'date&order=DESC',
-        'post__not_in' => get_option('sticky_posts'),
-        'post_status' => 'publish',
-        'order'    => 'DESC',
-        'posts_per_page' => -1
-      );
-      $result = new WP_Query($args);
-      if ($result->have_posts()) : ?>
-        <?php while ($result->have_posts()) : $result->the_post(); ?>
-          <section class="posts__post post">
-            <a href=<?php echo get_permalink(); ?>>
-              <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', TRUE); ?>" class="post__image">
-            </a>
-            <section class="info">
-              <a href=<?php echo get_permalink(); ?> class="post__title">
-                <?php the_title(); ?>
-              </a>
-              <p class="post__date">
-                <?php echo get_the_date(); ?>
-              </p>
-            </section>
-          </section>
-        <?php endwhile; ?>
-      <?php endif;
-      wp_reset_postdata(); ?>
-    </section>
+?>
+<?php
+get_header();
+?>
+<main class="not-found-container container thankyou">
+  <section class="not-found">
+    <p class="not-found__description--left"><a>Thank you</a> for<br class="break" /> getting in touch.</p>
   </section>
+</main>
+<section class="action">
+  <p class="action__description">We will get back to you soon.</p>
+  <a href="/" class="action__button action-button--lower">HOMEPAGE</a>
+</section>
+<section class="action-mobile action-thankyou">
+  <a href="/" class="action-mobile__button action-mobile__button--lower">HOMEPAGE</a>
 </section>
 <?php
+get_template_part('template-parts/blog');
 get_footer();
 ?>
